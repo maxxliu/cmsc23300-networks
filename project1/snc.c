02102545fa464d6b2b5cd0ae07614acc138fa6fb
Max Liu <maxliu@cs.uchicago.edu>
2018-10-09 22:05:08 -0500

    tcp works for a single message...still need to fix udp

--- project1/snc.c
+++ project1/snc.c
@@ -6,22 +6,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <netdb.h>
 
 
 void parseCommandArgs(int argc, char *argv[], int *flags);
 void errorInput();
+void errorOther();
+void error(char *msg);
+void runServer(int argc, char *argv[], int *flags);
+void runClient(int argc, char *argv[], int *flags);
 
 
 int main(int argc, char *argv[])
 {
-    
-
     int flags[3] = {0, 0, 0};
+
+    // parse the given commands and check if there is an error
     parseCommandArgs(argc, argv, flags);
+
+    // now we need to see if we should run as a client or as a server
+    if (flags[0]) {
+        // run as server
+        printf("running as server\n");
+        runServer(argc, argv, flags);
+    } else {
+        // run as client
+        printf("running as client\n");
+        runClient(argc, argv, flags);
+    }
 
     // first I want to check the command line arguments to make sure that the 
     // user has input a valid call to the program
@@ -107,4 +124,119 @@ void errorOther()
 {
     fprintf(stderr, "internal error\n");
     exit(1);
+}
+
+
+// exit with specific error message
+void error(char *msg)
+{
+    fprintf(stderr, "%s", msg);
+    exit(1);
+}
+
+
+// server side code
+void runServer(int argc, char *argv[], int *flags)
+{
+    int sockfd, newsockfd, portno, clilen;
+    char buffer[256];
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+
+    // first need to know to use either TCP or UDP
+    if (flags[1]) {
+        // use UDP
+        printf("using UDP\n");
+        sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    } else {
+        // use TCP
+        printf("using TCP\n");
+        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    }
+    if (sockfd < 0) {
+        error("ERROR opening socket\n");
+    }
+
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    portno = atoi(argv[argc - 1]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(portno);
+    //see if we need to set a hostname
+    if (flags[2]) {
+        // set the hostname to the user specified one
+        printf("setting server address to %s\n", argv[argc - 2]);
+        serv_addr.sin_addr.s_addr = inet_addr(argv[argc - 2]);
+    } else {
+        // set it to defualt
+        printf("setting server address to INADDR_ANY\n");
+        serv_addr.sin_addr.s_addr = INADDR_ANY;
+    }
+
+    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        error("ERROR on binding\n");
+    }
+    listen(sockfd, 5);
+    clilen = sizeof(cli_addr);
+    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+    if (newsockfd < 0) {
+        error("ERROR on accept\n");
+    }
+    bzero(buffer, 256);
+    n = read(newsockfd, buffer, 255);
+    if (n < 0) {
+        error("ERROR reading from socket\n");
+    }
+    printf("%s\n", buffer);
+}
+
+
+// client side code
+void runClient(int argc, char *argv[], int *flags)
+{
+    int sockfd, portno, n;
+
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    char buffer[256];
+
+    portno = atoi(argv[argc - 1]);
+
+    // first need to know to use either TCP or UDP
+    if (flags[1]) {
+        // use UDP
+        printf("using UDP\n");
+        sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    } else {
+        // use TCP
+        printf("using TCP\n");
+        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    }
+    if (sockfd < 0) {
+        error("ERROR opening socket\n");
+    }
+
+    // get the server
+    server = gethostbyname(argv[argc - 2]);
+    if (server == NULL) {
+        error("ERROR no such host\n");
+    }
+
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *) server->h_addr, 
+          (char *) &serv_addr.sin_addr.s_addr, 
+          server->h_length);
+    serv_addr.sin_port = htons(portno);
+
+    if (connect(sockfd, (struct sockaddr *) &serv_addr, 
+        sizeof(serv_addr)) < 0) {
+        error("ERROR connecting\n");
+    }
+    bzero(buffer,256);
+    fgets(buffer,255,stdin);
+    n = write(sockfd, buffer, strlen(buffer));
+    if (n < 0) {
+        error("ERROR writing to socket\n");
+    }
 }
